d4c58bf00797cd356441a0cad322872dccd82061
claudia <claudia@alpify.com>
2020-12-13 13:52:54 +0100

    add mocha & url.controllerTest

---
+++ test/unitTests/url.controllerTest.js
@@ -0,0 +1,11 @@
+// var assert = require('assert');
+const tinyURL = require('tinyurl');
+
+describe('Url controller', function() {
+    describe('#create()', function() {
+        it('should get shorter url without error', function(done) {
+
+            done()
+        });
+    });
+});
